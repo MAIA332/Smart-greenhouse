6525cb6e31493c9df57bd6db22de143dd522b938
Lukas Maia <67965680+MAIA332@users.noreply.github.com>
2023-11-08 16:39:25 -0300

    Update esp32_comunication_module.ino

--- esp32_comunication_module/esp32_comunication_module.ino
+++ esp32_comunication_module/esp32_comunication_module.ino
@@ -2,8 +2,8 @@
 #include <ESPAsyncWebSrv.h>
 #include <HTTPClient.h>
  
-const char* ssid = "NOTE 50";
-const char* password = "39866418820";
+const char* ssid = "Escritorio Mesh";
+const char* password = "nm12345678";
 const char* htmlContent = "<html><body><h1>Hello!, Esp32</h1><p>Projeto: IOT API para integrações em arduino ESP32</p><p>Documentação:https://github.com/MAIA332/IOTAPI</p>"
                           "</body></html>";
 
@@ -12,13 +12,11 @@ const char* htmlContentOn = "<html><body><h1>ON</h1>"
 
 const char* htmlContentOff = "<html><body><h1>OFF</h1>"
                           "</body></html>";
-
-int LED = 7;
-
+                          
 AsyncWebServer server(80);
  
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
  
   Serial.println();
   Serial.print("Conectando-se a ");
@@ -49,77 +47,27 @@ void setup() {
     request->send(200, "text", "Requisicao de dados em andamento...");
   });
 
-  // Rota para controlar a lampada termica:
+  //Rota para inicialização dos componentes
+  server.on("/test", HTTP_POST, [](AsyncWebServerRequest *request){
+    String postBody = request->arg("params");
+    Serial.println("Rota para inicialização dos componentes");
+    Serial.println(postBody);
+    request->send(200, "text/plain", postBody);
+  });
 
-  server.on("/ligar-lamp-t", HTTP_GET, [](AsyncWebServerRequest *request){    
-   
-    Serial.println("Ligando lampada termica...");
-    Serial.write('C');
+  server.on("/ligar-lamp-termica", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Rota para inicialização da lampada termica");
+    Serial.write("C");
+    request->send(200, "text/plain", "Rota para inicialização da lampada termica");
+  });
   
-    request->send(200, "text/html", htmlContentOn);
+  server.on("/desligar-lamp-termica", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Rota para desligamento da lampada termica");
+    Serial.write("D");
+    request->send(200, "text/plain", "Rota para desligamento da lampada termica");
   });
-
-  server.on("/desligar-lamp-t", HTTP_GET, [](AsyncWebServerRequest *request){
-   
-    Serial.println("Desligando lampada termica...");
-    Serial.write('D');
-    
-    request->send(200, "text/html", htmlContentOff);
-  });
-
-  // Rota para controlar os fans
-
-  server.on("/ligar-fan", HTTP_GET, [](AsyncWebServerRequest *request){
-    
-    Serial.println("Ligando fans...");
-    Serial.write('A');
-    
-    request->send(200, "text/html", htmlContentOn);
-  });
-
-  server.on("/desligar-fan", HTTP_GET, [](AsyncWebServerRequest *request){
-    
-    Serial.println("Desliando fans...");
-    Serial.write('B');
-
-    request->send(200, "text/html", htmlContentOff);
-  });
-
-  // Rota para controlar a lampada UV:
-
-  server.on("/ligar-lamp-uv", HTTP_GET, [](AsyncWebServerRequest *request){    
-   
-    Serial.println("Ligando lampada uv...");
-    Serial.write('E');
   
-    request->send(200, "text/html", htmlContentOn);
-  });
-
-  server.on("/desligar-lamp-uv", HTTP_GET, [](AsyncWebServerRequest *request){
-   
-    Serial.println("Desligando lampada uv...");
-    Serial.write('F');
-    
-    request->send(200, "text/html", htmlContentOff);
-  });
-
-  // Rota para controlar a bomba de irrigação:
-  server.on("/ligar-bomba", HTTP_GET, [](AsyncWebServerRequest *request){    
-   
-    Serial.println("Ligando lampada uv...");
-    Serial.write('G');
   
-    request->send(200, "text/html", htmlContentOn);
-  });
-
-  server.on("/desligar-bomba", HTTP_GET, [](AsyncWebServerRequest *request){
-   
-    Serial.println("Desligando lampada uv...");
-    Serial.write('H');
-    
-    request->send(200, "text/html", htmlContentOff);
-  });
-
   // Inicia o servidor
   server.begin();
 
@@ -138,5 +86,4 @@ void loop() {
   }
 
 }
-
 
